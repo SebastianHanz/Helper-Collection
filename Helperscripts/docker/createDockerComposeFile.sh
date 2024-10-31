45b7cd5810bb9ed76a03e93591a525993a52b3fe
Sebastian Hanz <SebastianHanz@gmx.de>
2024-10-31 17:20:12 +0100

    Update createDockerComposeFile.sh

--- Helperscripts/docker/createDockerComposeFile.sh
+++ Helperscripts/docker/createDockerComposeFile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 container_name=$1
+output_dir=$2
 
 if [ -z "$1" ]; then
 	echo -e "\nParameter fehlen!\n"
@@ -8,8 +9,12 @@ if [ -z "$1" ]; then
 	exit
 fi
 
+if [ -z "$2" ]; then
+	output_dir=$(pwd)/docker-compose.yml
+fi
+
 # Erstelle Docker-Compose-File
 echo -e "\nErstelle Docker-Compose-File in aktuellem Verzeichnis\n"
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock ghcr.io/red5d/docker-autocompose $container_name >> ./docker-compose.yml
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock ghcr.io/red5d/docker-autocompose $container_name > $output_dir
 echo -e "Fertig!\n"
 exit 0
